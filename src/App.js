477d8227a5069f5beed580a8a77493d929f19127
vishwasc20 <vishwasc20@gmail.com>
2021-05-28 22:58:38 +0530

    image recog initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,148 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import "@tensorflow/tfjs-backend-cpu";
 
 function App() {
+  const [isModelLoading, setIsModelLoading] = useState(false);
+  const [model, setModel] = useState(null);
+  const [imageURL, setImageURL] = useState(null);
+  const [results, setResults] = useState([]);
+  const [history, setHistory] = useState([]);
+
+  const imageRef = useRef();
+  const textInputRef = useRef();
+  const fileInputRef = useRef();
+
+  const loadModel = async () => {
+    setIsModelLoading(true);
+
+    try {
+      const model = await mobilenet.load();
+      setModel(model);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsModelLoading(false);
+    }
+  };
+
+  const uploadImage = (e) => {
+    const { files } = e.target;
+    let url = null;
+
+    if (files.length) {
+      url = URL.createObjectURL(files[0]);
+    }
+    setImageURL(url);
+  };
+
+  const identify = async () => {
+    textInputRef.current.value = "";
+    const results = await model.classify(imageRef.current);
+    setResults(results);
+  };
+
+  const handleOnChange = (e) => {
+    setImageURL(e.target.value);
+    setResults([]);
+  };
+
+  const triggerUpload = () => {
+    fileInputRef.current.click();
+  };
+
+  useEffect(() => {
+    loadModel();
+  }, []);
+
+  useEffect(() => {
+    if (imageURL) {
+      setHistory([imageURL, ...history]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [imageURL]);
+
+  if (isModelLoading) {
+    return <h2>Model Loading...</h2>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="header">Image Identification</h1>
+      <div className="inputHolder">
+        <input
+          type="file"
+          accept="image/*"
+          capture="camera"
+          className="uploadInput"
+          onChange={uploadImage}
+          ref={fileInputRef}
+        />
+        <button className="uploadImage" onClick={triggerUpload}>
+          Upload Image
+        </button>
+        <span className="or">OR</span>
+        <input
+          type="text"
+          placeholder="Paste image URL"
+          ref={textInputRef}
+          onChange={handleOnChange}
+        />
+      </div>
+      <div className="mainWrapper">
+        <div className="mainContent">
+          <div className="imageHolder">
+            {imageURL ? (
+              <img
+                src={imageURL}
+                alt="Upload Preview"
+                crossOrigin="anonymous"
+                ref={imageRef}
+              />
+            ) : null}
+          </div>
+          {results.length ? (
+            <div className="resultsHolder">
+              {results.map((result, index) => {
+                return (
+                  <div className="result" key={result.className}>
+                    <span className="name">{result.className}</span>
+                    <span className="confidence">
+                      Confidence level: {(result.probability * 100).toFixed(2)}%{" "}
+                      {index === 0 ? (
+                        <span className="bestGuess">Best Guess</span>
+                      ) : null}
+                    </span>
+                  </div>
+                );
+              })}
+            </div>
+          ) : null}
+        </div>
+        {imageURL ? (
+          <button className="button" onClick={identify}>
+            Identify Image
+          </button>
+        ) : null}
+      </div>
+      {history.length ? (
+        <div className="recentPredictions">
+          <h2>Recent Images</h2>
+          <div className="recentImages">
+            {history.map((image, index) => {
+              return (
+                <div className="recentPrediction" key={`${image}${index}`}>
+                  <img
+                    src={image}
+                    alt="Recent Prediction"
+                    onClick={() => setImageURL(image)}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
